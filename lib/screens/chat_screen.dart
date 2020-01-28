bc3954fb211c3123cbeeec957cc107d512de199a
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-01-28 19:05:11 +0100

    true

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -15,8 +15,47 @@ class _ChatScreenState extends State<ChatScreen> {
 
   _buildMessage(Message message, bool isMe) {
     return Container(
-      color: Color(0xFFFFEFEE),
-      child: Text(message.text),
+      margin: isMe ? EdgeInsets.only(
+        top: 8.0,
+        bottom: 8.0,
+        left: 80.0,
+        ) : EdgeInsets.only(
+          top: 8.0,
+          bottom: 8.0,
+          right: 80.0,
+        ),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        borderRadius: isMe ? BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          bottomLeft: Radius.circular(15.0)
+        ) : BorderRadius.only(
+          topRight: Radius.circular(15.0),
+          bottomRight: Radius.circular(15.0)
+        )
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            message.time, 
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600
+            ),
+          ),
+          SizedBox(height: 8.0,),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600
+            ),
+          ),
+      ],),
     );
   }
 
@@ -53,6 +92,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     topLeft: Radius.circular(30.0),
                     topRight: Radius.circular(30.0)),
                 child: ListView.builder(
+                  reverse: true,
                   padding: EdgeInsets.only(top: 15.0),
                   itemCount: messages.length,
                   itemBuilder: (BuildContext context, int index) {
