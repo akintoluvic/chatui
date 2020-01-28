3fa327bb3818e0557db3801d8f012c514919cb6f
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-01-28 15:39:04 +0100

    init

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/widgets/category_selector.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,7 +11,29 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text('Chats', style: TextStyle(
+          fontSize: 28.0,
+          fontWeight: FontWeight.bold
+        ),),
+        actions: <Widget>[
+          IconButton(
+          icon: Icon(Icons.search),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        ],
+      ),
+      body: Column(children: <Widget>[
+        CategorySelector(),
+      ],),
     );
   }
 }
