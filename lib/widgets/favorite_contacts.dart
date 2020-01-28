dda554530ea73175bfb8fcb760611be56abda66f
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-01-28 17:12:05 +0100

    init

--- lib/widgets/favorite_contacts.dart
+++ lib/widgets/favorite_contacts.dart
@@ -1,28 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/models/message_model.dart';
 
 class FavoriteContacts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Text(
-          'Favorite Contacts', 
-          style: TextStyle(
-            color: Colors.blueGrey,
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 1.0,
-          ),
-        ),
-        IconButton(
-          icon: Icon(
-            Icons.more_horiz,
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10.0
+      ),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
             ),
-            iconSize: 30.0,
-            color: Colors.blueGrey,
-           onPressed: () {},
-        )
-      ],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Favorite Contacts',
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.more_horiz,
+                  ),
+                  iconSize: 30.0,
+                  color: Colors.blueGrey,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 120.0,
+            child: ListView.builder(
+              padding: EdgeInsets.only(left: 10.0),
+              scrollDirection: Axis.horizontal,
+              itemCount: favorites.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 35.0,
+                        backgroundImage: AssetImage(favorites[index].imageUrl),
+                      ),
+                      SizedBox(height: 6.0,),
+                      Text(
+                        favorites[index].name, 
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),)
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
