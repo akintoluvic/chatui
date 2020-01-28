5a8c47649c195fb0ab8096198d099d16dc2fb1b2
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-01-28 19:22:17 +0100

    init

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -12,51 +12,67 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
   _buildMessage(Message message, bool isMe) {
-    return Container(
-      margin: isMe ? EdgeInsets.only(
-        top: 8.0,
-        bottom: 8.0,
-        left: 80.0,
-        ) : EdgeInsets.only(
-          top: 8.0,
-          bottom: 8.0,
-          right: 80.0,
+
+    final Container msg = Container(
+        margin: isMe
+            ? EdgeInsets.only(
+                top: 8.0,
+                bottom: 8.0,
+                left: 80.0,
+              )
+            : EdgeInsets.only(
+                top: 8.0,
+                bottom: 8.0,
+              ),
+        padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+        width: MediaQuery.of(context).size.width * 0.75,
+        decoration: BoxDecoration(
+            color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(15.0),
+                    bottomLeft: Radius.circular(15.0))
+                : BorderRadius.only(
+                    topRight: Radius.circular(15.0),
+                    bottomRight: Radius.circular(15.0))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              message.time,
+              style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              message.text,
+              style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600),
+            ),
+          ],
         ),
-      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
-      decoration: BoxDecoration(
-        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
-        borderRadius: isMe ? BorderRadius.only(
-          topLeft: Radius.circular(15.0),
-          bottomLeft: Radius.circular(15.0)
-        ) : BorderRadius.only(
-          topRight: Radius.circular(15.0),
-          bottomRight: Radius.circular(15.0)
-        )
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            message.time, 
-            style: TextStyle(
-              color: Colors.blueGrey,
-              fontSize: 16.0,
-              fontWeight: FontWeight.w600
-            ),
+      );
+    if(isMe) {
+      return msg;
+    }
+    return Row(children: <Widget>[
+      msg,
+      IconButton(
+            icon: message.isLiked 
+              ? Icon(Icons.favorite) 
+              : Icon(Icons.favorite_border),
+            iconSize: 30.0,
+            color: message.isLiked ? Theme.of(context).primaryColor : Colors.blueGrey,
+            onPressed: () {},
           ),
-          SizedBox(height: 8.0,),
-          Text(
-            message.text,
-            style: TextStyle(
-              color: Colors.blueGrey,
-              fontSize: 16.0,
-              fontWeight: FontWeight.w600
-            ),
-          ),
-      ],),
-    );
+    ]);
   }
 
   @override
